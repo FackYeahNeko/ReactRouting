68e6a16f30f0f406f78b569a51543621fb0898e1
FackYeahNeko <avefenixmas@gmail.com>
2023-11-28 12:58:59 +0100

    añadido el To Do y la pagina contador

---
+++ deconstructing.js
@@ -0,0 +1,19 @@
+const list = ['patata', 'manzanas0','peras'];
+const [a,b] = list;
+
+
+const obj = {
+    name: 'jorge',
+    age: 25,
+    country: 'ESP'
+}
+
+const {name: Nombre, age: Edad } = obj;  //Aqui asignas variables al contenido del objeto, ejemplo: el nombre jorge, que su key es name, se guardará en nombre.
+console.log (Nombre, Edad); 
+
+
+
+
+
+
+
---
+++ src/pages/Todo/todo.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"; //importas inicialmente la funcionalidad de usestate de react
+
+const todo {
+
+}
+
+
+function Button()  // creas el boton
+{  
+  const [clicks, setClicks] = useState(0);  // seteas los clicks iniciales
+  console.log("increaseBy", increaseBy); // aqui enseñas en consola el valor 
+
+  function handleClick() //creas la función handleClick
+  {
+    setClicks(clicks + increaseBy); // aqui usas el setclick, para cambiar la variable de clicks, y añadir el valor de incremento 
+  }
+  return (
+    <main>
+      <input
+        type="number" //creas el tipo de input , number limita el valor a numeros solo.
+        onChange={(ev) => setInc(Number(ev.target.value))} //cuando detecta cambios en el input, se ejecuta la función setInc
+        value={increaseBy}
+      />
+      <button
+        onClick={handleClick} //llamas a la funcion handleClick cuando haces click (on click)
+        >
+        Clicked {clicks} times
+      </button>
+    </main>
+  );
+}
+
+export default Button; // exportamos el componente Button
